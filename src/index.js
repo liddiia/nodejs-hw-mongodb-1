a550b9214f8324f40c1ea8bde3ab2dc40e606d30
liddiia <liddabojko@gmail.com>
2025-01-05 16:29:10 +0200

    Summury

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { setupServer } from "./server.js";
 import { initMongoConnection } from "./db/initMongoConnection.js";
-//06iEtnAQsqVDDMRP
+
 const boostrap = async ()=>{
     //спочатку підключаємось до бази
     await initMongoConnection();
--- src/server.js
+++ src/server.js
@@ -1,65 +1,25 @@
 import express from "express";
 import cors from "cors";
-// import pino from "pino-http";
+
+// import { logger } from "./middelwares/logger.js";
+import { notFoundHandler } from "./middelwares/notFoundHandler.js";
 
 import { getEnvVar } from "./utils/getEnvVar.js";
-
-//import {getContacts, getContactsById} from "./services/contacts.js";
-import * as contactServices from "./services/contacts.js";
+import contactsRouter from "./routers/contacts-routers.js";
+import { errorHandler } from "./middelwares/errorHandler.js";
 
 export const setupServer =()=>{
 
     const app = express();
 
     app.use(cors());
-    app.use(express.json());
+    app.use(express.json()); //відповідає за обробку запиту в форматі json
+    // app.use(logger);
 
-    // usi kontakti otrimuemo:
+app.use("/contacts", contactsRouter);
 
-app.get("/contacts",async (req,res)=>{
-    const data = await contactServices.getContacts();
-
-    res.json({
-        status:200,
-        message:"Successfully found contacts",
-        data});
-
-});
-
-// zapit odnogo kontaktu:
-
-app.get("/contacts/:id", async (req,res)=>{
-
-const {id} = req.params;
-
-const data = await contactServices.getContactsById(id);
-if(!data){
-return res.status(404).json({
-    status: 404,
-    message: `Contact with id = ${id} not found`
-});
-};
-
-res.json({
-    status:200,
-    message:`Successfully found contact with id=${id}`,
-    data,
-});
-});
-
-app.use((req,res)=>{
-    res.status(404).json({
-        message: `${req.url} not found`
-    });
-});
-    app.use((error, req, res, next)=>{
-        res.status(500).json({
-            message: `${req.url} not found`,
-            error: error.message,
-            next
-        });
-
-});
+app.use(notFoundHandler);
+    app.use(errorHandler);
 const port = Number(getEnvVar("PORT", 3000) );
 app.listen(port, ()=>{console.log(`Server is running on port ${port}`);
 });
