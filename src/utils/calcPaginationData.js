e33a63184c9c322d532bfc644cea3cc9f01a8c38
liddiia <liddabojko@gmail.com>
2025-01-09 12:47:18 +0200

    summury

--- src/utils/calcPaginationData.js
+++ src/utils/calcPaginationData.js
@@ -3,9 +3,6 @@ export const calcPaginationData = ({totalItems, page, perPage}) => {
  const hasNextPage = page < totalPages;
  const hasPreviousPage = page > 1;
   return {
-    //дані можна вивести тут як ...ginationData, зараз виводяться в контроллері
-    // page,
-    // perPage,
     totalPages,
     hasNextPage,
     hasPreviousPage,
