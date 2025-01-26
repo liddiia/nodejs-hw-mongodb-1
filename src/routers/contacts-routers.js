cf29666e21170a050597980875cf5adaa5aca168
liddiia <liddabojko@gmail.com>
2025-01-26 08:14:21 +0200

    Summury

--- src/routers/contacts-routers.js
+++ src/routers/contacts-routers.js
@@ -35,6 +35,7 @@ contactsRouter.post(
 contactsRouter.patch(
   '/:id',
   isValidId,
+  upload.single('photo'),
   validationBody(contactsUpdateSchema),
   ctrlWrapper(contactsController.patchContactController),
 );
